24dcce914cec8ea59db52ccc00e9da5af982fd5c
aung win thant <aungwinthant@laelar.com>
2018-12-23 10:42:48 +0630

    fix navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -27,15 +27,15 @@
                     </li>
                 @else
                     <li class="nav-item dropdown">
-                        <a href="/transactions" class="dropdown-item">Dashboard</a>
-                        <a href="{{route('history',Auth::user()->id)}}" class="dropdown-item">History</a>
-                        <a href="{{route('currency_exchange')}}" class="dropdown-item">Currency Exchange</a>
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a  class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            
+                            <a  class="dropdown-item" href="/transactions" class="dropdown-item">Dashboard</a>
+                            <a  class="dropdown-item" href="{{route('history',Auth::user()->id)}}" class="dropdown-item">History</a>
+                            <a  class="dropdown-item" href="{{route('currency_exchange')}}" class="dropdown-item">Currency Exchange</a>
+                            <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
