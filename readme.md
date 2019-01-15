1e99ff2d6fc44c59b33c6ebf0e56257eec552f08
aung win thant <aungwinthant@laelar.com>
2019-01-15 12:42:01 +0630

    add readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Note
+
+Currency Exchange will not work in local as it is a cron job in Kernel.php file.
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-5 bg-black">
     <div class="container">
-      <p class="m-0 text-center text-white small">Made with <i class="fas fa-heart"></i> in my apartment 2018</p>
+      <p class="m-0 text-center text-white small">Made with <i class="fas fa-heart"></i> in my apartment</p>
     </div>
     <!-- /.container -->
   </footer>
